61241a59d1c940ac8f20bd81e88bad71b3450c45
Johannes Hertenstein <johannes@nimius.net>
2018-11-30 00:19:31 +0100

    BUGFIX | Fix InstanceCreationVisitor not checking class type

--- src/Parser/InstanceCreationVisitor.php
+++ src/Parser/InstanceCreationVisitor.php
@@ -9,7 +9,7 @@ class InstanceCreationVisitor extends NamespaceCollectingVisitor
 
     public function enterNode(Node $node)
     {
-        if ($node instanceof Node\Expr\New_) {
+        if ($node instanceof Node\Expr\New_ && $node->class instanceof Node\Name) {
             $this->namespaces[] = $node->class->toString();
         }
     }
