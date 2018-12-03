4158c9e8e6cd82f328fb10a620f1b87f73081a91
Johannes Hertenstein <johannes@nimius.net>
2018-12-04 00:44:12 +0100

    BUGFIX | Only collect fully qualified references to classes

--- src/Parser/Visitor/FullyQualifiedReference.php
+++ src/Parser/Visitor/FullyQualifiedReference.php
@@ -7,7 +7,7 @@ use PhpParser\Node;
  * This includes class extensions, interface usages, instance creations etc.
  *
  * This Visitor is best used in combination with PhpParser\NodeVisitor\NameResolver
- * as that visitor converts imported namespace usages into qully qualified names.
+ * as that visitor converts imported namespace usages into fully qualified names.
  * In that combination this visitor will extract every direct reference to a class.
  */
 class FullyQualifiedReference extends NamespaceCollectingVisitor
@@ -16,7 +16,9 @@ class FullyQualifiedReference extends NamespaceCollectingVisitor
     public function enterNode(Node $node)
     {
         if ($node instanceof Node\Name\FullyQualified) {
-            $this->namespaces[] = (string) $node;
+            if ($node->isFullyQualified() && class_exists((string) $node)) {
+                $this->namespaces[] = (string) $node;
+            }
         }
     }
 
