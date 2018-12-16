5b6f66208ae875dc48a730495f927137aa3222c3
Johannes Hertenstein <johannes@nimius.net>
2018-12-16 14:35:38 +0100

    CLEANUP | Fix code style violations

--- src/Parser/Visitor/DocBlockTypeAnnotations.php
+++ src/Parser/Visitor/DocBlockTypeAnnotations.php
@@ -22,7 +22,9 @@ class DocBlockTypeAnnotations extends NamespaceCollectingVisitor
             $this->lastClass = $node->namespacedName->toString();
         } elseif ($node instanceof Node\Stmt\ClassMethod && $this->lastClass) {
             $contextFactory = new \phpDocumentor\Reflection\Types\ContextFactory();
-            $context = $contextFactory->createFromReflector(new ReflectionMethod($this->lastClass, (string) $node->name));
+            $context = $contextFactory->createFromReflector(
+                new ReflectionMethod($this->lastClass, (string) $node->name)
+            );
             if ($node->hasAttribute('comments')) {
                 $this->extractDocBlocks((array)$node->getAttribute('comments'), $context);
             }
