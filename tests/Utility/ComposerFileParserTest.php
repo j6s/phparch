63fe43d8a8ec6e3725afd40a802bcc04732562fa
Johannes Hertenstein <johannes@nimius.net>
2019-05-19 13:19:36 +0200

    TASK | Add test for ComposerFileParser

--- tests/Utility/ComposerFileParserTest.php
+++ tests/Utility/ComposerFileParserTest.php
@@ -1,15 +1,68 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: j
- * Date: 5/19/19
- * Time: 12:56 PM
- */
-
 namespace J6s\PhpArch\Tests\Utility;
 
 
-class ComposerFileParserTest
+use J6s\PhpArch\Tests\TestCase;
+use J6s\PhpArch\Utility\ComposerFileParser;
+
+class ComposerFileParserTest extends TestCase
 {
+
+    /** @var ComposerFileParser */
+    private $subject;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->subject = new ComposerFileParser(__DIR__ . '/Mock/composer.json');
+    }
+
+    public function testExtractsName(): void
+    {
+        $this->assertEquals(
+            'j6s/phparch-mock',
+            $this->subject->getName()
+        );
+    }
+
+    public function testExtractsNamespaces(): void
+    {
+        $this->assertEquals(
+            [ 'J6s\\PhpArchMock' ],
+            $this->subject->getNamespaces(false)
+        );
+        $this->assertEquals(
+            [ 'J6s\\PhpArchMock', 'J6s\\PhpArchMock\\Tests' ],
+            $this->subject->getNamespaces(true)
+        );
+    }
+
+    public function testShouldExtractDependencies(): void
+    {
+        $this->assertEquals(
+            [ 'thecodingmachine/safe' ],
+            $this->subject->getDirectDependencies(false)
+        );
+        $this->assertEquals(
+            [ 'thecodingmachine/safe', 'phpunit/phpunit' ],
+            $this->subject->getDirectDependencies(true)
+        );
+    }
+
+    public function testExtractsNamespacesForPackageName()
+    {
+        $this->assertContains(
+            'Safe\\',
+            $this->subject->autoloadableNamespacesForRequirements([ 'thecodingmachine/safe' ], false)
+        );
+    }
+
+    public function testDeepRequirementNamespacesContainsDepenenciesOfDependencies()
+    {
+        $namespaces = $this->subject->getDeepRequirementNamespaces(true);
+
+        // phpunit/phpunit depends on doctrine/instantiator
+        $this->assertContains('Doctrine\\Instantiator\\', $namespaces);
+    }
 
 }
